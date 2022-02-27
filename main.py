36f54e52f3b52b4e6c9ad03ba12645d9c0166255
derrick921213 <dlin12457@gmail.com>
2022-02-27 17:59:33 +0800

    Fix

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import os
 from simplepam import authenticate
 from flask import Flask, session,redirect,url_for,escape,request,render_template
-from temp.gen import gen_code,check_code
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     if 'username' in session:
-        return 'Logged in as %s <p><a href="%s" target="_self">Logout</a></p>' % (escape(session['username']),'/logout')
+        return 'Logged in as %s <p><a href="%s" target="_self">Logout</a></p>' % (escape(session['username']),url_for('logout'))
     return redirect(url_for('login'))
 
 
@@ -40,4 +39,4 @@ def logout():
 app.secret_key = os.urandom(24)
 
 if __name__ == '__main__':
-    app.run(debug='True')
+    app.run(host='0.0.0.0',debug='True')
