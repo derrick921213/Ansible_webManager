071be4d458a1390dcf2fca9fe1a08c8a5b48c75b
derrick921213 <dlin12457@gmail.com>
2022-03-08 20:18:45 +0800

    add typing

--- Utils/account.py
+++ Utils/account.py
@@ -1,7 +1,7 @@
 import grp
 from simplepam import authenticate
 
-def isAdmin(username,admin_group):
+def isAdmin(username: str,admin_group: str) -> str:
     return (username,admin_group in [g.gr_name for g in grp.getgrall() if username in g.gr_mem] if True else False)
-def canLogin(username,password,admin_group):
+def canLogin(username: str,password: str,admin_group:str) -> str:
     return authenticate(isAdmin(username,'sudo' if admin_group == 'sudo' else 'user')[0] if isAdmin(username,'sudo' if admin_group == 'sudo' else 'user')[1] else '' ,password)
